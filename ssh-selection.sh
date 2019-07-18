755b97a05e94ce7ce27a2413ef184770a372589f
Sjors Ottjes <sjorsottjes@gmail.com>
2019-07-18 11:20:20 +0200

    Create ssh-selection.sh

---
+++ ssh-selection.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HEIGHT=18
+WIDTH=48
+CHOICE_HEIGHT=8
+TITLE="SSH"
+MENU="Choose a server:"
+
+OPTIONS=(
+1 "some-server.com"
+2 "another-server.com"
+)
+
+CHOICE=$(dialog --clear \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+    1)
+        ssh user@some-server.com
+        echo -ne "\033]0;$*\007" # reset terminal title
+        ;;
+    2)
+        ssh user@another-server.com
+        echo -ne "\033]0;$*\007" # reset terminal title
+        ;;
+esac
